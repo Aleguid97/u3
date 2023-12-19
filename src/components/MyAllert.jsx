fc993ec6d0aa4dfdd5db385c2b66cc9959ba21c8
Aleguid97 <aleguid97@gmail.com>
2023-12-19 16:43:23 +0100

    CommitFinale2

--- src/components/MyAllert.jsx
+++ src/components/MyAllert.jsx
@@ -5,14 +5,14 @@ const MyAlert = () => {
   const [showAlert, setShowAlert] = useState(true);
 
   useEffect(() => {
-    // Imposta showAlert su false dopo 3 secondi (puoi modificare il tempo a tuo piacimento)
+    
     const timeout = setTimeout(() => {
       setShowAlert(false);
     }, 5000);
 
-    // Pulisci il timeout quando il componente si smonta
+ 
     return () => clearTimeout(timeout);
-  }, []); // L'array vuoto [] indica che useEffect viene eseguito solo una volta al caricamento
+  }, []);
 
   return (
     <div>
